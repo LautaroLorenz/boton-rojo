24ebd5c9a9959808e51d8700358c59a3bf0de60e
jim-llorenz <lautaro.lorenz@mercadolibre.com>
2022-09-23 21:50:21 -0300

    mas retos

--- src/app/models/challenges/challenges.model.ts
+++ src/app/models/challenges/challenges.model.ts
@@ -4,12 +4,16 @@ const easy: string[] = [
   'hacer origami',
   'cantá una canción a capela',
   'dibujar un perro comiendo queso',
+  'tomar un vaso de agua de una vez',
 ];
 const hard: string[] = [
   'tomar una cuchara de picante',
   'comer banana con mayonesa',
   'caminar con una cuchara en la boca que tenga un huevo durante diez pasos',
   'jugar con un amigo o amiga a lanzar un huevo',
+  'ver un capitulo del marginal entero',
+  'barrer saltando en una pata ',
+  'gritar de chill',
 ];
 
 function getRandomInt(max: number) {
